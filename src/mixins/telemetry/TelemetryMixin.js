bd64910df2b2e55ac030a168c06c70c25759b1e8
Nick Hallman <Nick.Hallman@D2L.com>
2022-09-22 14:28:11 -0400

    initial push

--- src/mixins/telemetry/TelemetryMixin.js
+++ src/mixins/telemetry/TelemetryMixin.js
@@ -9,6 +9,14 @@ export const TelemetryMixin = options => superClass => class TelemetryMixinClass
 		return options.properties;
 	}
 
+	get fireOnClose() {
+		return options.fireOnClose;
+	}
+
+	get debounce() {
+		return options.debounce;
+	}
+
 	constructor() {
 		super();
 		const requiredKeys = [
@@ -22,16 +30,20 @@ export const TelemetryMixin = options => superClass => class TelemetryMixinClass
 			const foundMissing = requiredKeys.find(key => !Object.keys(options).includes(key));
 			throw new Error(`Telemetry options must have all required keys. Missing ${foundMissing}`);
 		}
-		if( !options.actions.every(action => typeof action === 'symbol') ) {
+		if(!options.actions.every(action => typeof action === 'symbol') ) {
 			throw new Error(`Telemetry actions must be symbols`);
 		}
-		if( !options.properties.every(prop => typeof prop === 'symbol') ) {
+		if(!options.properties.every(prop => typeof prop === 'symbol') ) {
 			throw new Error(`Telemetry properties must be symbols`);
 		}
 
 		this.client = new d2lTelemetryBrowserClient.Client({
 			endpoint: options.endpoint
 		});
+
+		if(this.fireOnClose === true) {
+			this.eventQueue = [];
+		}
 	}
 
 	_handleTelemetryEvent(e) {
@@ -51,14 +63,45 @@ export const TelemetryMixin = options => superClass => class TelemetryMixinClass
 				.setSourceId(options.sourceId)
 				.setBody(eventBody);
 
-			this.client.logUserEvent(event);
+			if(options.middleware) {
+				options.middleware(event);
+			}
+
+			if(!this.fireOnClose || !this.debounce || this.debounce === 0) {
+				this.client.logUserEvent(event);
+			} else {
+				this._storeEventAndDebounce(event)
+			}
+
 		} else {
 			throw new Error("Telemetry event actions and properties must be from the defined symbol list");
+		}
+	}
+
+	_storeEventAndDebounce(event) {
+		this.eventQueue.push(event);
+		if(this.debounce && this.debounce !== 0) {
+			if(this.debounceTimeout) clearTimeout(this.debounceTimeout);
+			this.debounceTimeout = setTimeout(
+				() => this.eventQueue.forEach(storedEvent => this.client.logUserEvent(storedEvent)),
+				this.debounce
+			);
+		}
+	}
+
+	_handleVisibilityChange(e) {
+		if(e.visibilityState === "hidden"){
+			this.eventQueue.forEach(event =>
+				this.client.logUserEvent(event)
+			);
 		}
 	}
 
 	connectedCallback() {
 		super.connectedCallback();
 		this.addEventListener('d2l-telemetry-event', this._handleTelemetryEvent);
+		if(options.fireOnClose || (options.debounce && options.debounce !== 0)) {
+			this.addEventListener('visibilitychange', this._handleVisibilityChange);
+		}
 	}
 }
