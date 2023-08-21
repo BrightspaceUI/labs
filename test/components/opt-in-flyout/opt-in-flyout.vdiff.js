7c93df8d7fe7e391cfd626786b8d182d7985aceb
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-08-21 12:58:31 -0400

    add vdiff tests for opt-in-flyout (#102)

---
+++ test/components/opt-in-flyout/opt-in-flyout.vdiff.js
@@ -0,0 +1,153 @@
+import '../../../src/components/opt-in-flyout/flyout-impl.js';
+import '../../../src/components/opt-in-flyout/opt-in-flyout.js';
+import '../../../src/components/opt-in-flyout/opt-out-flyout.js';
+import { expect, fixture, html, oneEvent, sendKeysElem } from '@brightspace-ui/testing';
+
+const opts = { viewport: { height: 450 } };
+
+describe('flyout-impl', () => {
+
+	it('closed', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl flyout-title="Flyout Impl">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('closed-focus', async() => {
+		const elem = await fixture(html`
+			<d2l-labs-opt-in-flyout-impl flyout-title="Flyout Impl" opened>
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		const tabButton = elem.shadowRoot.querySelector('#flyout-tab');
+		sendKeysElem(tabButton, 'press', 'Enter');
+		await oneEvent(elem, 'flyout-closed');
+		await expect(document).to.be.golden();
+	});
+
+	it('opened', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl flyout-title="Flyout Impl" opened>
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('opened-focus', async() => {
+		const elem = await fixture(html`
+			<d2l-labs-opt-in-flyout-impl flyout-title="Flyout Impl">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		const tabButton = elem.shadowRoot.querySelector('#flyout-tab');
+		sendKeysElem(tabButton, 'press', 'Enter');
+		await oneEvent(elem, 'flyout-opened');
+		await expect(document).to.be.golden();
+	});
+
+	it('short description only', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				opened
+				short-description="short description">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('long description only', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				opened
+				long-description="long description">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('short and long description', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				opened
+				long-description="long description"
+				short-description="short description">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('tutorial link only', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				opened
+				tutorial-link="https://www.d2l.com">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('help-docs link only', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				help-docs-link="https://www.d2l.com"
+				opened>
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('tutorial and help-docs links', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout-impl
+				flyout-title="Flyout Impl"
+				help-docs-link="https://www.d2l.com"
+				opened
+				tutorial-link="https://www.d2l.com">
+			</d2l-labs-opt-in-flyout-impl>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+});
+
+describe('opt-in-flyout', () => {
+
+	it('opened', async() => {
+		await fixture(html`
+			<d2l-labs-opt-in-flyout
+				flyout-title="Opt-In Flyout"
+				opened>
+			</d2l-labs-opt-in-flyout>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+});
+
+describe('opt-out-flyout', () => {
+
+	it('opened', async() => {
+		await fixture(html`
+			<d2l-labs-opt-out-flyout flyout-title="Opt-Out Flyout" opened>
+			</d2l-labs-opt-out-flyout>
+		`, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('opted out', async() => {
+		const elem = await fixture(html`
+			<d2l-labs-opt-out-flyout flyout-title="Opt-Out Flyout" opened>
+			</d2l-labs-opt-out-flyout>
+		`, opts);
+		const optOutButton = elem.shadowRoot.querySelector('d2l-labs-opt-in-flyout-impl')
+			.shadowRoot.querySelector('#opt-out-button');
+		sendKeysElem(optOutButton, 'press', 'Enter');
+		await expect(document).to.be.golden();
+	});
+
+});
--- test/components/opt-in-flyout/opt-out-dialog.test.js
+++ test/components/opt-in-flyout/opt-out-dialog.test.js
@@ -2,7 +2,8 @@ import '../../../src/components/opt-in-flyout/opt-out-dialog.js';
 import '../../../src/components/opt-in-flyout/opt-out-reason.js';
 import { clickElem, expect, fixture, html, oneEvent } from '@brightspace-ui/testing';
 
-const setSelection = (reasonSelector, selectIndex) => {
+const setSelection = (elem, selectIndex) => {
+	const reasonSelector = elem.shadowRoot.querySelector('d2l-labs-opt-out-reason-selector');
 	const select = reasonSelector.shadowRoot.querySelector('select');
 	select.selectedIndex = selectIndex;
 	reasonSelector._reasonSelected();
@@ -13,119 +14,37 @@ const setFeedback = (flyout, value) => {
 	feedback.value = value;
 };
 
-const clickDone = (flyout) => {
-	const doneButton = flyout.shadowRoot.querySelector('d2l-button[primary]');
+const clickDone = (elem) => {
+	const doneButton = elem.shadowRoot.querySelector('d2l-button[primary]');
 	clickElem(doneButton);
 };
 
 describe('opt-out-dialog', () => {
 
-	describe('defaults', () => {
+	describe('events', () => {
 
-		let flyout, reasonSelector;
+		let elem;
 
 		beforeEach(async() => {
-			flyout = await fixture(html`<d2l-labs-opt-out-dialog></d2l-labs-opt-out-dialog>`);
-			reasonSelector = flyout.shadowRoot.querySelector('d2l-labs-opt-out-reason-selector');
+			elem = await fixture(html`<d2l-labs-opt-out-dialog></d2l-labs-opt-out-dialog>`);
 		});
 
-		it('should contain a title', () => {
-			const message = flyout.shadowRoot.querySelector('#title-label');
-			expect(message.textContent).to.contain('Let us know how to improve!');
-		});
-
-		it('should contain d2l-labs-opt-out-reason-selector', () => {
-			const selector = flyout.shadowRoot.querySelector('d2l-labs-opt-out-reason-selector');
-			expect(selector).to.exist;
-		});
-
-		it('should contain feedback text input box', () => {
-			const feedback = flyout.shadowRoot.querySelector('#feedback');
-			expect(feedback).to.exist;
-		});
-
-		it('should contain cancel button', () => {
-			const cancelButton = flyout.shadowRoot.querySelector('d2l-button:not([primary])');
-			expect(cancelButton).to.exist;
-		});
-
-		it('should fire confirm event when reason selected and done clicked (no feedback)', async() => {
-			setSelection(reasonSelector, 1);
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
+		it('should dispatch "confirm" event when reason selected and done clicked (no feedback)', async() => {
+			setSelection(elem, 1);
+			clickDone(elem);
+			const e = await oneEvent(elem, 'confirm');
 			expect(e.detail.reason).to.equal('PreferOldExperience');
 			expect(e.detail.feedback).to.equal('');
 		});
 
-		it('should fire confirm event when reason selected and done clicked with feedback', async() => {
+		it('should dispatch "confirm" event when reason selected and done clicked with feedback', async() => {
 			const textFeedback = 'not a fan';
-			setSelection(reasonSelector, 1);
-			setFeedback(flyout, textFeedback);
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
+			setSelection(elem, 1);
+			setFeedback(elem, textFeedback);
+			clickDone(elem);
+			const e = await oneEvent(elem, 'confirm');
 			expect(e.detail.reason).to.equal('PreferOldExperience');
 			expect(e.detail.feedback).to.equal(textFeedback);
-		});
-
-	});
-
-	describe('options specified', () => {
-
-		let flyout, reasonSelector;
-
-		beforeEach(async() => {
-			flyout = await fixture(html`
-				<d2l-labs-opt-out-dialog>
-					<d2l-labs-opt-out-reason key="test-1" text="Test Option 1"></d2l-labs-opt-out-reason>
-					<d2l-labs-opt-out-reason key="test-2" text="Test Option 2"></d2l-labs-opt-out-reason>
-				</d2l-labs-opt-out-dialog>
-			`);
-			reasonSelector = flyout.shadowRoot.querySelector('d2l-labs-opt-out-reason-selector');
-		});
-
-		it('should contain the first option', async() => {
-			setSelection(reasonSelector, 1);
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
-			expect(e.detail.reason).to.equal('test-1');
-		});
-
-		it('should contain the second option', async() => {
-			setSelection(reasonSelector, 2);
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
-			expect(e.detail.reason).to.equal('test-2');
-		});
-
-	});
-
-	describe('reason hidden', () => {
-
-		it('fires confirm with no reason', async() => {
-			const flyout = await fixture(html`
-				<d2l-labs-opt-out-dialog hide-reason>
-					<d2l-labs-opt-out-reason key="test-1" text="Test Option 1"></d2l-labs-opt-out-reason>
-					<d2l-labs-opt-out-reason key="test-2" text="Test Option 2"></d2l-labs-opt-out-reason>
-				</d2l-labs-opt-out-dialog>
-			`);
-			const hiddenReason = flyout.shadowRoot.querySelector('div[hidden] > #reason-selector');
-			expect(hiddenReason).to.exist;
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
-			expect(e.detail.reason).to.equal('');
-		});
-
-	});
-
-	describe('feedback hidden', () => {
-
-		it('fires confirm with no feedback', async() => {
-			const flyout = await fixture(html`<d2l-labs-opt-out-dialog hide-feedback></d2l-labs-opt-out-dialog>`);
-			const hiddenFeedback = flyout.shadowRoot.querySelector('div[hidden] > #feedback');
-			expect(hiddenFeedback).to.exist;
-			clickDone(flyout);
-			const e = await oneEvent(flyout, 'confirm');
-			expect(e.detail.feedback).to.equal('');
 		});
 
 	});
