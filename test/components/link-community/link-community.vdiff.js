626a0ac9943ea3ed69542ac639a091d08c68bcef
Nick Hallman <Nick.Hallman@D2L.com>
2023-09-08 12:46:40 -0400

    add vis diffs

---
+++ test/components/link-community/link-community.vdiff.js
@@ -0,0 +1,38 @@
+
+import '../../../src/components/link-community/link-community.js';
+import { expect, fixture, html, oneEvent, sendKeysElem } from '@brightspace-ui/testing';
+
+const opts = { viewport: { height: 450 } };
+
+const langArticleMap = {
+	'en': 4545,
+	'ar-SA': 10489,
+	'es-MX': 9214,
+	'pt-BR': 9978,
+	'nl-NL': 7833,
+	'fr-CA': 6739
+};
+
+describe('link-community', () => {
+	it('Should render a link in link mode', async() => {
+		await fixture(
+			html`<d2l-labs-link-community
+				article-map="${JSON.stringify(langArticleMap)}"
+				type="link"
+				text="Link To Community"
+			></d2l-labs-link-community>`
+			, opts);
+		await expect(document).to.be.golden();
+	});
+
+	it('Should render a button in button mode', async() => {
+		await fixture(
+			html`<d2l-labs-link-community
+				article-map="${JSON.stringify(langArticleMap)}"
+				type="button"
+				text="Link To Community"
+			></d2l-labs-link-community>`
+			, opts);
+		await expect(document).to.be.golden();
+	});
+});
