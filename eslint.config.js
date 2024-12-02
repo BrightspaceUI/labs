e621963a831cb82f91d1b07faa3d1fb5db40a2a3
GZolla <43836485+GZolla@users.noreply.github.com>
2024-12-02 08:19:52 -0800

    From review

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,6 @@
 import { addExtensions, litConfig, setDirectoryConfigs, testingConfig } from 'eslint-config-brightspace';
 
-export default [
-	{ ignores: ['.vdiff/'] },
-	...addExtensions(setDirectoryConfigs(
-		litConfig,
-		{ test: testingConfig },
-	), ['.js', '.html']),
-	{
-		files: ['demo/**/**.html'],
-		rules: { 'no-console': 0 }
-	}
-];
+export default setDirectoryConfigs(
+	addExtensions(litConfig, ['.js', '.html']),
+	{ test: testingConfig }
+);
