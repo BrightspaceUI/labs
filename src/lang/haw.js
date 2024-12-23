e89754410880b1d6f314236d33f4dc803cba4db5
tie.d2l.dev <TieUser@d2l.com>
2024-12-23 18:10:08 +0000

    Translation Updates

---
+++ src/lang/haw.js
@@ -0,0 +1,46 @@
+export default {
+	"components:accessibilityDisabilitySimulator:blurLevel": "Blur Level", // Label for input slider that users can use to change the level of blurriness
+	"components:accessibilityDisabilitySimulator:colorblindAchromatopsia": "Achromatopsia", // Disability simulation option for users with Achromatiopsia colorblindness
+	"components:accessibilityDisabilitySimulator:colorblindDeuteranopia": "Deuteranopia", // Disability simulation option for users with Deuteranopia colorblindness
+	"components:accessibilityDisabilitySimulator:colorblindProtanopia": "Protanopia", // Disability simulation option for users with Protanopia colorblindness
+	"components:accessibilityDisabilitySimulator:colorblindTritanopia": "Tritanopia", // Disability simulation option for users with Tritanopia colorblindness
+	"components:accessibilityDisabilitySimulator:colorblindness": "Colorblindness", // Disability simulation option for users with Colorblindness
+	"components:accessibilityDisabilitySimulator:keyboardOnly": "Keyboard Only", // Disability simulation option for users who can only use the keyboard for navigation
+	"components:accessibilityDisabilitySimulator:lowVision": "Low Vision", // Disability simulation option for users with a limited ability to see
+	"components:accessibilityDisabilitySimulator:noVision": "No Vision", // Disability simulation option for users without the ability to see
+	"components:accessibilityDisabilitySimulator:none": "ʻAʻohe", // Disability simulation option that represents having no disability
+	"components:accessibilityDisabilitySimulator:screenreaderAndTabAlert": "Enable your screenreader and tab into the content", // Alert that prompts the user to use a screenreader and tabbing to view content
+	"components:accessibilityDisabilitySimulator:simulationType": "Simulation type:", // Label for the disability simulation options
+	"components:attributePicker:addValue": "Click to add value {value}", // Label for screen readers to inform user that they can click to add the given value
+	"components:attributePicker:limitReached": "You've reached the limit of {value} selected values", //  Tooltip that appears when the user has reach the maximum amount of attributes
+	"components:attributePicker:minimumRequirement": "At least one value must be set", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
+	"components:attributePicker:removeValue": "Kaomi, kaomi i hope, a i ʻole ke kī holoi e wehe i ka mea {value}", // Label to inform user how they can remove the given value
+	"components:checkboxDrawer:checkboxCollapsed": "Checkbox collapsed", // Read by screen readers when focusing the collapsed (unchecked) checkbox, with the inner content hidden
+	"components:checkboxDrawer:checkboxExpanded": "Checkbox expanded", // Read by screen readers when focusing the expanded (checked) checkbox, with the inner content shown
+	"components:optInFlyout:cancel": "Hoʻopau",
+	"components:optInFlyout:close": "Pani i kēia kamaʻilio",
+	"components:optInFlyout:done": "Pau",
+	"components:optInFlyout:feedbackChooseReason": "-- E koho i kahi kumu --",
+	"components:optInFlyout:feedbackLabel": "He aha kā mākou e hana ai i mea āu e makemake ai e hoʻohana?",
+	"components:optInFlyout:feedbackReasonJustCheckingSomething": "Ke hoʻololi wale nei e nānā i kekahi mea",
+	"components:optInFlyout:feedbackReasonLabel": "Makemake ʻoe e haʻi mai iā mākou no ke aha ʻoe e hoʻi ai?",
+	"components:optInFlyout:feedbackReasonMissingFeature": "Ua nele kahi hiʻona aʻu e hoʻohana nei",
+	"components:optInFlyout:feedbackReasonNotReadyForSomethingNew": "ʻAʻole ia he manawa maikaʻi noʻu e hoʻāʻo i kēia mana",
+	"components:optInFlyout:feedbackReasonOther": "'ē aʻe",
+	"components:optInFlyout:feedbackReasonPreferOldExperience": "Manaʻo wau he ʻoi aku ka maikaʻi o ka mana kahiko",
+	"components:optInFlyout:feedbackTitle": "E ʻike iā mākou pehea e hoʻomaikaʻi ai!",
+	"components:optInFlyout:helpMessage": "E heluhelu i kā mākou * palapala kōkua * e hoʻomaka ai!",
+	"components:optInFlyout:leaveOff": "Waiho ia",
+	"components:optInFlyout:leaveOn": "Waiho ia",
+	"components:optInFlyout:openOptIn": "E wehe i ke kamaʻilio koho",
+	"components:optInFlyout:openOptOut": "E wehe i ke kamaʻilio haʻalele",
+	"components:optInFlyout:turnOff": "E hoʻopau",
+	"components:optInFlyout:turnOn": "E hoʻā",
+	"components:optInFlyout:tutorialAndHelpMessage": "E nānā i kā mākou * kumu aʻo * a heluhelu i kā mākou ~ palapala kōkua ~ e hoʻomaka!",
+	"components:optInFlyout:tutorialMessage": "E nānā i kā mākou * kumu aʻo * e kōkua iā ʻoe e hoʻomaka!",
+	"components:pagination:amountPerPage": "{helu} no kēlā me kēia ʻaoʻao",
+	"components:pagination:currentPage": "Helu ʻaoʻao {pageNumber} o {maxPageNumber}",
+	"components:pagination:nextPage": "ʻAoʻao aʻe",
+	"components:pagination:previousPage": "ʻaoʻao mua",
+	"components:pagination:resultsPerPage": "Nā hualoaʻa no kēlā me kēia ʻaoʻao"
+};
