f8313ae835ea36b597ab503528647f3efc837dda
tie.d2l.dev <TieUser@d2l.com>
2025-02-24 20:49:04 +0000

    Translation Updates

--- src/lang/haw.js
+++ src/lang/haw.js
@@ -1,22 +1,22 @@
 export default {
-	"components:accessibilityDisabilitySimulator:blurLevel": "Blur Level", // Label for input slider that users can use to change the level of blurriness
+	"components:accessibilityDisabilitySimulator:blurLevel": "ʻĀpana Blur", // Label for input slider that users can use to change the level of blurriness
 	"components:accessibilityDisabilitySimulator:colorblindAchromatopsia": "Achromatopsia", // Disability simulation option for users with Achromatiopsia colorblindness
 	"components:accessibilityDisabilitySimulator:colorblindDeuteranopia": "Deuteranopia", // Disability simulation option for users with Deuteranopia colorblindness
 	"components:accessibilityDisabilitySimulator:colorblindProtanopia": "Protanopia", // Disability simulation option for users with Protanopia colorblindness
 	"components:accessibilityDisabilitySimulator:colorblindTritanopia": "Tritanopia", // Disability simulation option for users with Tritanopia colorblindness
-	"components:accessibilityDisabilitySimulator:colorblindness": "Colorblindness", // Disability simulation option for users with Colorblindness
-	"components:accessibilityDisabilitySimulator:keyboardOnly": "Keyboard Only", // Disability simulation option for users who can only use the keyboard for navigation
-	"components:accessibilityDisabilitySimulator:lowVision": "Low Vision", // Disability simulation option for users with a limited ability to see
-	"components:accessibilityDisabilitySimulator:noVision": "No Vision", // Disability simulation option for users without the ability to see
+	"components:accessibilityDisabilitySimulator:colorblindness": "Makapō kala", // Disability simulation option for users with Colorblindness
+	"components:accessibilityDisabilitySimulator:keyboardOnly": "Keyboard Wale", // Disability simulation option for users who can only use the keyboard for navigation
+	"components:accessibilityDisabilitySimulator:lowVision": "Nānā Haʻahaʻa", // Disability simulation option for users with a limited ability to see
+	"components:accessibilityDisabilitySimulator:noVision": "ʻAʻohe ʻike", // Disability simulation option for users without the ability to see
 	"components:accessibilityDisabilitySimulator:none": "ʻAʻohe", // Disability simulation option that represents having no disability
-	"components:accessibilityDisabilitySimulator:screenreaderAndTabAlert": "Enable your screenreader and tab into the content", // Alert that prompts the user to use a screenreader and tabbing to view content
-	"components:accessibilityDisabilitySimulator:simulationType": "Simulation type:", // Label for the disability simulation options
-	"components:attributePicker:addValue": "Click to add value {value}", // Label for screen readers to inform user that they can click to add the given value
-	"components:attributePicker:limitReached": "You've reached the limit of {value} selected values", //  Tooltip that appears when the user has reach the maximum amount of attributes
-	"components:attributePicker:minimumRequirement": "At least one value must be set", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
+	"components:accessibilityDisabilitySimulator:screenreaderAndTabAlert": "E ho'ā i kāu mea heluhelu pale a kau i ka ʻike", // Alert that prompts the user to use a screenreader and tabbing to view content
+	"components:accessibilityDisabilitySimulator:simulationType": "ʻAno hoʻohālike:", // Label for the disability simulation options
+	"components:attributePicker:addValue": "Kaomi e hoʻohui i ka waiwai {value}", // Label for screen readers to inform user that they can click to add the given value
+	"components:attributePicker:limitReached": "Ua hiki ʻoe i ka palena o {value} koʻikoʻi i koho ʻia", //  Tooltip that appears when the user has reach the maximum amount of attributes
+	"components:attributePicker:minimumRequirement": "Pono e hoʻonoho i hoʻokahi waiwai", // Tooltip that appears when no values have been set and we need to inform the user that this is an invalid state
 	"components:attributePicker:removeValue": "Kaomi, kaomi i hope, a i ʻole ke kī holoi e wehe i ka mea {value}", // Label to inform user how they can remove the given value
-	"components:checkboxDrawer:checkboxCollapsed": "Checkbox collapsed", // Read by screen readers when focusing the collapsed (unchecked) checkbox, with the inner content hidden
-	"components:checkboxDrawer:checkboxExpanded": "Checkbox expanded", // Read by screen readers when focusing the expanded (checked) checkbox, with the inner content shown
+	"components:checkboxDrawer:checkboxCollapsed": "Ua hāʻule ka pahu pahu", // Read by screen readers when focusing the collapsed (unchecked) checkbox, with the inner content hidden
+	"components:checkboxDrawer:checkboxExpanded": "Hoʻonui ʻia ka pahu pahu", // Read by screen readers when focusing the expanded (checked) checkbox, with the inner content shown
 	"components:navigation:back": "Ke kua",
 	"components:navigation:next": "Aʻe",
 	"components:navigation:previous": "Mamua",
