f3e55135ee3c98a76e0931e5ec919d923b82ca5a
Jake Prouse <jprouse@desire2learn.com>
2025-05-01 11:36:36 -0400

    Remove viewport tests

--- test/components/view-toggle/view-toggle.vdiff.js
+++ test/components/view-toggle/view-toggle.vdiff.js
@@ -5,31 +5,15 @@ const basicFixture = html`<d2l-labs-view-toggle
 toggle-options='[{"text":"Bananas","val":"overall"},{"text":"Minions","val":"minios"},{"text":"Pyjamas","val":"subject"}]'
 text="Toggle: "></d2l-labs-view-toggle>`;
 
-const noWrapFixture = html`<d2l-labs-view-toggle
-toggle-options='[{"text":"Bananas","val":"overall"},{"text":"Minions","val":"minios"},{"text":"Pyjamas","val":"subject"},{"text":"Elephant","val":"longmemory"},{"text":"Tiger","val":"fast"}]'
-text="Toggle: "></d2l-labs-view-toggle>`;
-
-const viewports = [
-	{ viewport: { height: 800, width: 1230 } },
-	{ viewport: { height: 800, width: 865 } },
-	{ viewport: { height: 800, width: 767 } },
-	{ viewport: { height: 800, width: 614 } },
-	{ viewport: { height: 800, width: 430 } }
-];
-
 describe('d2l-labs-view-toggle', () => {
-	it('should not wrap', async() => {
-		const el = await fixture(noWrapFixture, { viewport: { height: 800, width: 430 } });
+	it('should render', async() => {
+		const el = await fixture(basicFixture);
 		await expect(el).to.be.golden();
 	});
 
-	describe('viewports', () => {
-		for (const viewport of viewports) {
-			it(`should render on ${viewport.viewport.width}px width`, async() => {
-				const el = await fixture(basicFixture, { viewport: viewport.viewport });
-				await expect(el).to.be.golden();
-			});
-		}
+	it('should not wrap', async() => {
+		const el = await fixture(basicFixture, { viewport: { height: 800, width: 430 } });
+		await expect(el).to.be.golden();
 	});
 
 	describe('rtl', () => {
