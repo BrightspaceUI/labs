4691ec7d7de94e4e252ca20af59f42d5eaf31167
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2025-05-28 11:32:19 -0400

    feat: GAUD-7980 - Clean up media-player docs (#656)
    * Move demo components and include them
    * Update README

--- test/components/media-player/media-player.vdiff.js
+++ test/components/media-player/media-player.vdiff.js
@@ -18,7 +18,7 @@ describe('d2l-labs-media-player', () => {
 			html`
 			<d2l-labs-media-player
 				src="./test/components/media-player/videos/1_lego.webm"
-				poster="./demo/components/media-player/sample-poster.png"
+				poster="./demo/components/media-player/static/sample-poster.png"
 				media-type="video">
 			</d2l-labs-media-player>`
 		);
@@ -40,7 +40,7 @@ describe('d2l-labs-media-player', () => {
 		const elem = await fixture(
 			html`
 			<d2l-labs-media-player src="./test/components/media-player/videos/1_lego.webm" media-type="video">
-				<track src="./demo/components/media-player/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+				<track src="./demo/components/media-player/static/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
 			</d2l-labs-media-player>`
 		);
 		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
@@ -51,7 +51,7 @@ describe('d2l-labs-media-player', () => {
 		const elem = await fixture(
 			html`
 			<d2l-labs-media-player src="./test/components/media-player/audio/applause.mp3" media-type="audio">
-				<track src="./demo/components/media-player/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+				<track src="./demo/components/media-player/static/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
 			</d2l-labs-media-player>`
 		);
 		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
