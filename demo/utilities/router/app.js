29c8c6fb8772560c631f46e2dacf509dfdcc874c
Dave Batiste <dave.batiste@d2l.com>
2025-07-11 15:51:53 -0400

    Update the router demo to include a menu containing d2l-menu-item-link.

--- demo/utilities/router/app.js
+++ demo/utilities/router/app.js
@@ -3,6 +3,10 @@ import { css, html, LitElement } from 'lit';
 import { basePath } from './util.js';
 import { RouteReactor } from '../../../src/utilities/router/index.js';
 
+import '@brightspace-ui/core/components/menu/menu.js';
+import '@brightspace-ui/core/components/menu/menu-item.js';
+import '@brightspace-ui/core/components/menu/menu-item-link.js';
+
 export class App extends LitElement {
 	static styles = css`
 		:host {
@@ -33,6 +37,12 @@ export class App extends LitElement {
 						<li><a href="${basePath}/places">Places</a></li>
 					</ul>
 				</nav>
+				<hr>
+				<d2l-menu label="Astronomy" style="max-width: 400px;">
+					<d2l-menu-item-link href="${basePath}/home" text="Home"></d2l-menu-item-link>
+					<d2l-menu-item-link href="${basePath}/people" text="People"></d2l-menu-item-link>
+					<d2l-menu-item-link href="${basePath}/places" text="Places"></d2l-menu-item-link>
+				</d2l-menu>
 			</aside>
 			<main>${this.route.renderView()}</main>
 		`;
