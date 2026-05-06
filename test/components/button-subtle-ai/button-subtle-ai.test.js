9af28be41592f91a94360286c0aa88a7cc9730c4
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-05-06 13:35:00 -0400

    Adds test file for the d2l-labs-button-subtle-ai component

---
+++ test/components/button-subtle-ai/button-subtle-ai.test.js
@@ -0,0 +1,10 @@
+import '../../../src/components/button-subtle-ai/button-subtle-ai.js';
+import { runConstructor } from '@brightspace-ui/testing';
+
+describe('button-subtle-ai', () => {
+	describe('constructor', () => {
+		it('should construct', () => {
+			runConstructor('d2l-labs-button-subtle-ai');
+		});
+	});
+});
