c42b25b0c0213c7c377fcb9c9feb12bc72537009
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-05-06 16:54:36 -0400

    Adds styles to the component and more unit tests

--- src/components/button-subtle-ai/button-subtle-ai.js
+++ src/components/button-subtle-ai/button-subtle-ai.js
@@ -1,11 +1,37 @@
 
 import '@brightspace-ui/core/components/button/button-subtle.js';
-import { html, LitElement } from 'lit';
+import { css, html, ifDefined, LitElement } from 'lit';
 
 class ButtonSubtleAI extends LitElement {
+	static get properties() {
+		return {
+			/**
+			 * ACCESSIBILITY: REQUIRED: Text for the button
+			 * @type {string}
+			 */
+			text: { type: String, reflect: true }
+		};
+	}
+
+	static get styles() {
+		return [css`
+			:host d2l-button-subtle {
+				border: 2px solid transparent;
+				border-radius: 6px;
+				background:
+					linear-gradient(var(--d2l-theme-background-color-base), var(--d2l-theme-background-color-base)) padding-box,
+					linear-gradient(to top left, var(--d2l-color-celestine), var(--d2l-color-fluorite-plus-1)) border-box;
+
+				&:focus-within {
+					background: none;
+				}
+			}
+		`];
+	}
+
 	render() {
 		return html `
-			<d2l-button-subtle></d2l-button-subtle>
+			<d2l-button-subtle text="${ifDefined(this.text)}" icon="tier1:ai"></d2l-button-subtle>
 		`;
 	}
 }
