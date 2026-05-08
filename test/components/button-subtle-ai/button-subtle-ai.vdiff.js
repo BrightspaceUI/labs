32b51f8cfef726a5898d375aa433eceb57a6b324
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-05-08 10:30:20 -0400

    Removes redundant test file and add more vdiff tests

--- test/components/button-subtle-ai/button-subtle-ai.vdiff.js
+++ test/components/button-subtle-ai/button-subtle-ai.vdiff.js
@@ -1,13 +1,24 @@
 import '../../../src/components/button-subtle-ai/button-subtle-ai.js';
-import { expect, fixture, focusElem } from '@brightspace-ui/testing';
+import { expect, fixture, focusElem, hoverElem } from '@brightspace-ui/testing';
 import { html } from 'lit';
 
 describe('button-subtle-ai', () => {
 	describe('states', () => {
+		it('normal', async() => {
+			const element = await fixture(html`<d2l-labs-button-subtle-ai text="Test"></d2l-labs-button-subtle-ai>`);
+			await expect(element).to.be.golden();
+		});
+
 		it('focus', async() => {
-			const element = await fixture(html`<d2l-labs-button-subtle-ai text="Test"></d2l-labs-button-subtle-ai>`, { viewport: { width: 150, height: 100 } });
+			const element = await fixture(html`<d2l-labs-button-subtle-ai text="Test"></d2l-labs-button-subtle-ai>`);
 			await focusElem(element.shadowRoot.querySelector('d2l-button-subtle'));
-			await expect(document).to.be.golden();
+			await expect(element).to.be.golden();
+		});
+
+		it('hover', async() => {
+			const element = await fixture(html`<d2l-labs-button-subtle-ai text="Test"></d2l-labs-button-subtle-ai>`);
+			await hoverElem(element.shadowRoot.querySelector('d2l-button-subtle'));
+			await expect(element).to.be.golden();
 		});
 	});
 });
