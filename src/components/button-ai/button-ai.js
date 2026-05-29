562ff3dbea416a138cc54b3e6d59c4c54feddb3e
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-05-28 22:32:25 -0400

    Adds disabled styles and behavior

--- src/components/button-ai/button-ai.js
+++ src/components/button-ai/button-ai.js
@@ -3,6 +3,7 @@ import { css, html, LitElement } from 'lit';
 import { ButtonMixin } from '@brightspace-ui/core/components/button/button-mixin.js';
 import { buttonStyles } from '@brightspace-ui/core/components/button/button-styles.js';
 import { getUniqueId } from '@brightspace-ui/core/helpers/uniqueId.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { SlottedIconMixin } from '@brightspace-ui/core/components/icons/slotted-icon-mixin.js';
 
@@ -46,6 +47,14 @@ class ButtonAI extends SlottedIconMixin(ButtonMixin(LitElement)) {
 					linear-gradient(to top left, var(--d2l-color-fluorite-plus-1), var(--d2l-color-celestine-plus-1)) border-box;
 			}
 
+			:host([disabled]) button {
+				background:
+					linear-gradient(var(--d2l-theme-background-color-base), var(--d2l-theme-background-color-base)) padding-box,
+					linear-gradient(to top left, var(--d2l-color-fluorite-plus-1), var(--d2l-color-celestine-plus-1)) border-box;
+				cursor: default;
+				opacity: 0.5;
+			}
+
 			button:hover:not([disabled]) .content,
 			button:focus:not([disabled]) .content {
 				color: var(--d2l-theme-text-color-interactive-hover);
@@ -72,7 +81,11 @@ class ButtonAI extends SlottedIconMixin(ButtonMixin(LitElement)) {
 
 	render() {
 		return html `
-			<button ?disabled=${this.disabled} id="${this.#buttonId}" class="d2l-label-text">
+			<button
+				aria-disabled="${ifDefined(this.disabled && this.disabledTooltip ? 'true' : undefined)}"
+				?disabled=${this.disabled && !this.disabledTooltip}
+				id="${this.#buttonId}"
+				class="d2l-label-text">
 				${this._renderIcon()}
 				<span class="content">${this.text}</span>
 			</button>
