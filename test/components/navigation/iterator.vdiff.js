dfc1146c9795a592f4fad2fea62726f748973bff
Aidan Frost <33972075+aid848@users.noreply.github.com>
2026-06-12 11:55:28 -0700

    HAI-3197 update tests/demo/vdiff for NavigationIterator

--- test/components/navigation/iterator.vdiff.js
+++ test/components/navigation/iterator.vdiff.js
@@ -10,7 +10,8 @@ describe('d2l-labs-navigation-iterator', () => {
 			{ name: 'hide-text', template: html`<d2l-labs-navigation-iterator hide-text></d2l-labs-navigation-iterator>` },
 			{ name: 'disabled', template: html`<d2l-labs-navigation-iterator previous-disabled next-disabled></d2l-labs-navigation-iterator>` },
 			{ name: 'custom-text', template: html`<d2l-labs-navigation-iterator previous-text="Back" next-text="Forward"></d2l-labs-navigation-iterator>` },
-			{ name: 'slot', template: html`<d2l-labs-navigation-iterator>Hello</d2l-labs-navigation-iterator>` }
+			{ name: 'slot', template: html`<d2l-labs-navigation-iterator>Hello</d2l-labs-navigation-iterator>` },
+			{ name: 'no-highlight-border', template: html`<d2l-labs-navigation-iterator no-highlight-border></d2l-labs-navigation-iterator>` }
 		].forEach(({ name, template }) => {
 			it(name, async() => {
 				const elem = await fixture(template, { viewport: { width: 366 } });
